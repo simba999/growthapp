5037f8d2ea62060c44b0911aaffbbb8c3823be72
simba999 <simba1512@yahoo.com>
2018-06-09 11:49:39 +0530

    config changes

--- android/app/src/main/java/com/balehuapp/MainApplication.java
+++ android/app/src/main/java/com/balehuapp/MainApplication.java
@@ -12,10 +12,8 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
 import java.util.Arrays;
 import java.util.List;
-import com.airbnb.android.react.maps.MapsPackage;
 
 
 public class MainApplication extends Application implements ReactApplication {
